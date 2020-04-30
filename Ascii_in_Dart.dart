f1cbc20bd22371833b0ae8a40be4f2d240cabc0e
code-and-desgin <64597287+code-and-desgin@users.noreply.github.com>
2020-05-01 05:15:33 +0530

    Create Ascii_in_Dart.dart

---
+++ Ascii_in_Dart.dart
@@ -0,0 +1,9 @@
+// Program to find the ASCII value of a character
+void main()
+{
+  var character = 'A';
+  print("It's Ascii Value ${character.codeUnitAt(0)}");
+}
+
+// Output
+// It's Ascii Value 65
